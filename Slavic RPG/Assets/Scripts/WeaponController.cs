efd6e3a1e5efbe6a22b82ece8b6efd05009fcad6
KelobKicjow <114358909+KelobDev@users.noreply.github.com>
2024-12-06 18:47:16 +0100

    jakieś małe dodadki

--- Slavic RPG/Assets/Scripts/WeaponController.cs
+++ Slavic RPG/Assets/Scripts/WeaponController.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class WeaponController : MonoBehaviour
 {
     public PlayerController player;
+    public Transform playerPos;
+    Rigidbody2D rb;
+    public float playerDetectRange = 5f;;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
